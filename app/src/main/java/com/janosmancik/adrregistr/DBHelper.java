1148bf8894b3e522f8155d2a72f70e13bcb004f8
Jan Osmancik <jan.osmancik.st@vsb.cz>
2017-12-07 11:44:38 +0100

    DBHelper

--- app/src/main/java/com/janosmancik/adrregistr/DBHelper.java
+++ app/src/main/java/com/janosmancik/adrregistr/DBHelper.java
@@ -12,78 +12,143 @@ import java.util.ArrayList;
 
 public class DBHelper extends SQLiteOpenHelper{
 
-    public static final String DATABASE_NAME = "MyDBName03.db";
-    public static final String CONTACTS_TABLE_NAME = "contacts";
-    public static final String CONTACTS_COLUMN_ID = "id";
-    public static final String CONTACTS_COLUMN_NAME = "name";
-    public static final String CONTACTS_COLUMN_TYPE = "type";
-    public static final String CONTACTS_COLUMN_COST = "cost";
+    //The Android's default system path of your application database.
+    String DB_PATH = NULL;
 
-    public static ArrayList<String> arrayList = new ArrayList<String>();
+    private static String DB_NAME = "latky";
 
-    public DBHelper(Context context)
-    {
-        super(context, DATABASE_NAME, null, 1);
+    private SQLiteDatabase myDataBase;
+
+    private final Context myContext;
+    /**
+     * Constructor
+     * Takes and keeps a reference of the passed context in order to access to the application assets and resources.
+     * @param context
+     */
+    public DataBaseHelper(Context context) {
+
+        super(context, DB_NAME, null, 1);
+        this.myContext = context;
+        this.DB_PATH = "/data/data/" + context.getPackageName() + "/databases/";
+        Log.e("Path 1", DB_PATH);
+    }
+
+    /**
+     * Creates a empty database on the system and rewrites it with your own database.
+     * */
+    public void createDataBase() throws IOException{
+
+        boolean dbExist = checkDataBase();
+
+        if(dbExist){
+            //do nothing - database already exist
+        }else{
+
+            //By calling this method and empty database will be created into the default system path
+            //of your application so we are gonna be able to overwrite that database with our database.
+            this.getReadableDatabase();
+
+            try {
+
+                copyDataBase();
+
+            } catch (IOException e) {
+
+                throw new Error("Error copying database");
+
+            }
+        }
+
+    }
+
+    /**
+     * Check if the database already exist to avoid re-copying the file each time you open the application.
+     * @return true if it exists, false if it doesn't
+     */
+    private boolean checkDataBase(){
+
+        SQLiteDatabase checkDB = null;
+
+        try{
+            String myPath = DB_PATH + DB_NAME;
+            checkDB = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+
+        }catch(SQLiteException e){
+
+            //database does't exist yet.
+
+        }
+
+        if(checkDB != null){
+
+            checkDB.close();
+
+        }
+
+        return checkDB != null ? true : false;
+    }
+
+    /**
+     * Copies your database from your local assets-folder to the just created empty database in the
+     * system folder, from where it can be accessed and handled.
+     * This is done by transfering bytestream.
+     * */
+    private void copyDataBase() throws IOException{
+
+        //Open your local db as the input stream
+        InputStream myInput = myContext.getAssets().open(DB_NAME);
+
+        // Path to the just created empty db
+        String outFileName = DB_PATH + DB_NAME;
+
+        //Open the empty db as the output stream
+        OutputStream myOutput = new FileOutputStream(outFileName);
+
+        //transfer bytes from the inputfile to the outputfile
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myInput.read(buffer))>0){
+            myOutput.write(buffer, 0, length);
+        }
+
+        //Close the streams
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+
+    }
+
+    public void openDataBase() throws SQLException{
+
+        //Open the database
+        String myPath = DB_PATH + DB_NAME;
+        myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+
+    }
+
+    @Override
+    public synchronized void close() {
+
+        if(myDataBase != null)
+            myDataBase.close();
+
+        super.close();
+
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        //db.execSQL("create table contacts " + "(id integer primary key, name text)");
-        db.execSQL("CREATE TABLE contacts " + "(id INTEGER PRIMARY KEY, name TEXT, type INTEGER, cost INTEGER)");
+
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS contacts");
-        onCreate(db);
+
     }
 
-    public boolean insertContact(String name)
-    {
-        SQLiteDatabase db = this.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
-        contentValues.put("name", name);
-        db.insert("contacts", null, contentValues);
-        return true;
-    }
+    // Add your public helper methods to access and get content from the database.
+    // You could return cursors by doing "return myDataBase.query(....)" so it'd be easy
+    // to you to create adapters for your views.
 
-    //Cursor representuje vracena data
-    public Cursor getData(int id){
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res =  db.rawQuery("select * from contacts where id=" + id + "", null);
-        //Cursor res =  db.rawQuery( "select * from contacts LIMIT 1 OFFSET "+id+"", null );
-        return res;
-    }
-
-    public boolean updateContact (Integer id, String name)
-    {
-        //TODO update zaznam
-        return true;
-    }
-
-    public void setAllContacs()
-    {
-        arrayList.clear();
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res =  db.rawQuery( "select * from contacts", null );
-        res.moveToFirst();
-
-        while(res.isAfterLast() == false){
-            arrayList.add(res.getString(res.getColumnIndex(CONTACTS_COLUMN_NAME)));
-            res.moveToNext();
-        }
-    }
-
-    public ArrayList<String> getAllContacsName()
-    {
-        return arrayList;
-    }
-
-    public void removeAll()
-    {
-        SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(CONTACTS_TABLE_NAME, "1", null);
-    }
-
-
+}
 }
