95d57dc100f68124bd8c895344f2b38e9314fbbb
Jan Osmančík <j.osmancik@gmail.com>
2017-12-12 01:11:44 +0100

    some sh*t
    sdk error after version changed

---
+++ src/main/java/com/janosmancik/adrregistr/SubstanceObjectModel.java
@@ -0,0 +1,70 @@
+package com.janosmancik.adrregistr;
+
+public class SubstanceObjectModel {
+
+    String un, kemler, latka, bezd, trida, ohrozeni, ochrana, znecisteni, pomoc, kod;
+
+    public SubstanceObjectModel() {
+
+    }
+    // UN property
+
+    public String getUn() {
+        return un;
+    }
+
+    public void setUn(String val) {
+        this.un = val;
+    }
+
+    // kemler property
+
+    public String getKemler() {
+        return kemler;
+    }
+
+    public void setKemler(String val) {
+        this.kemler = val;
+    }
+
+    //latka property
+
+    public String getLatka() {
+        return latka;
+    }
+
+    public void setLatka(String val) {
+        this.latka = val;
+    }
+
+    public String getBezd() {
+        return bezd;
+    }
+
+    public String getTrida() {
+        return trida;
+    }
+
+    public String getOhrozeni() {
+        return ohrozeni;
+    }
+
+    public String getOchrana() {
+        return ochrana;
+    }
+
+    public String getZnecisteni() {
+        return znecisteni;
+    }
+
+    public String getPomoc() {
+        return pomoc;
+    }
+
+    public String getKod() {
+        return kod;
+    }
+
+
+
+}
