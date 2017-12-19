a5e773e463b6c5fee83610185e4091981e41e6ef
Jan Osmancik <jan.osmancik.st@vsb.cz>
2017-12-19 16:35:45 +0100

    Vision improoved

--- src/main/java/com/janosmancik/adrregistr/MainActivity.java
+++ src/main/java/com/janosmancik/adrregistr/MainActivity.java
@@ -8,9 +8,11 @@ import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
@@ -38,6 +40,14 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+        Button btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), RecognitionActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
         // Construct the data source
