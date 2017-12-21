ac5d98a021ef7608c51dbb56479faccad72050ae
Jan Osmancik <jan.osmancik.st@vsb.cz>
2017-12-21 06:34:19 +0100

    geolocation

--- src/main/java/com/janosmancik/adrregistr/DisplaySubstance.java
+++ src/main/java/com/janosmancik/adrregistr/DisplaySubstance.java
@@ -1,9 +1,17 @@
 package com.janosmancik.adrregistr;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -22,7 +30,8 @@ public class DisplaySubstance extends Activity {
     TextView name;
     TextView ochrana;
     TextView ohrozeni;
-
+    Button callButton;
+    Location loc;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,10 +56,9 @@ public class DisplaySubstance extends Activity {
             try {
                 mydb = new DBHelper(getApplicationContext());
                 latka = mydb.getData(value);
-            } catch(Exception e) {
+            } catch (Exception e) {
                 Log.w("Error", e.getMessage());
-            }
-            finally {
+            } finally {
                 Log.i("Finally", "");
             }
         }
@@ -67,11 +75,55 @@ public class DisplaySubstance extends Activity {
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_CALL);
 
-                intent.setData(Uri.parse("tel:123"));
+                Intent intent1 = intent.setData(Uri.parse("tel:123"));
+                startActivity(intent1);
+            }
+        });
+
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        // Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                loc = location;
+                Log.i("location", String.valueOf(loc.getLatitude()));
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                 startActivity(intent);
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.INTERNET}, 10);
+        } else return;
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 10:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    configureButton();
+                return;
+
+        }
+    }
+
+    public void configureButton() {
+        Button callButton = findViewById(R.id.callBtn);
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                locationManager.requestLocationUpdates("gps", 10000, 20, locationListener);
             }
         });
     }
-
-
 }
+}
+
